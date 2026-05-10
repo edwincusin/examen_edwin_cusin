6bf00a32ad2460f9ed54f0b17416df3362cbd5a5
EDWINCSN <c.edwin32@hotmail.com>
2026-05-09 20:03:42 -0500

    EGCA: creacion clase TestVendedor

---
+++ src/test/java/com/krakedev/examen_edwin_cusin/test/TestVendedor.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_edwin_cusin.test;
+
+import com.krakedev.examen_edwin_cusin.entidades.Vendedor;
+
+public class TestVendedor {
+
+	public static void main(String[] args) {
+		
+		Vendedor vendedor=new Vendedor("1753081056");
+		System.out.println(vendedor.toString());
+	}
+
+}
---
+++ src/test/java/com/krakedev/examen_edwin_cusin/testJUnit/TestJUnitVendedor.java
@@ -0,0 +1,5 @@
+package com.krakedev.examen_edwin_cusin.testJUnit;
+
+public class TestJUnitVendedor {
+
+}
