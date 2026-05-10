7aef6a339f27a45ff58b055c6aeac20304a43e55
EDWINCSN <c.edwin32@hotmail.com>
2026-05-09 19:50:24 -0500

    EGCA: commit inicial

---
+++ src/main/java/com/krakedev/examen_edwin_cusin/ExamenEdwinCusinApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_edwin_cusin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenEdwinCusinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenEdwinCusinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakedev/examen_edwin_cusin/ExamenEdwinCusinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_edwin_cusin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenEdwinCusinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
