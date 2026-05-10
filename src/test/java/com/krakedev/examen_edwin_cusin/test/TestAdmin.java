b70e88f5c0e29952d91ce29b7f7e63874913c0a2
EDWINCSN <c.edwin32@hotmail.com>
2026-05-09 21:26:45 -0500

    EGCA: se provoca el nullpinter exception y agregar link de video en videos.txt

--- src/test/java/com/krakedev/examen_edwin_cusin/test/TestAdmin.java
+++ src/test/java/com/krakedev/examen_edwin_cusin/test/TestAdmin.java
@@ -9,41 +9,51 @@ public class TestAdmin {
 
 	public static void main(String[] args) {
 		
-		//	Instanciar AdminVentas 
-		AdminVentas av = new AdminVentas();
 		
-		//Crear un Vendedor, un VendedorComision y un VendedorMixto 
-		//Setear los datos necesarios para calcular los sueldos 
-		Vendedor vendedor = new Vendedor("1753081056");
-		vendedor.setComisionPorVenta(5.2);
-		vendedor.setNumeroVentas(20);
-		vendedor.setSueldoFijo(450);
+		
+		
+		try {
+//			Instanciar AdminVentas 
+			AdminVentas av = new AdminVentas();
+			//Crear un Vendedor, un VendedorComision y un VendedorMixto 
+			//Setear los datos necesarios para calcular los sueldos 
+			Vendedor vendedor = new Vendedor(null);               //<------------- aqui provoco el nullpinterexception
+			vendedor.setComisionPorVenta(5.2);
+			vendedor.setNumeroVentas(20);
+			vendedor.setSueldoFijo(450);
 
-		VendedorComision vendedorComision = new VendedorComision("10053081056");
-		vendedorComision.setComisionPorVenta(2);
-		vendedorComision.setNumeroVentas(40);
-		vendedorComision.setSueldoFijo(450);
+			VendedorComision vendedorComision = new VendedorComision("10053081056");
+			vendedorComision.setComisionPorVenta(2);
+			vendedorComision.setNumeroVentas(40);
+			vendedorComision.setSueldoFijo(450);
 
-		VendedorMixto vendedorMixto = new VendedorMixto("0012345678");
-		vendedorMixto.setComisionPorVenta(1);
-		vendedorMixto.setNumeroVentas(50);
-		vendedorMixto.setSueldoFijo(450);
-		
-		//	Agregar los vendedores usando el método agregar 
-		av.agregar(vendedor);
-		av.agregar(vendedorComision);
-		av.agregar(vendedorMixto);
-		
-		//Probar el método calcularSueldo con los tres vendedores 
-		System.out.println("Sueldo vendedor normal: " + av.calcularSueldo("1753081056"));
-		System.out.println("Solo Comision --- > vendedor comision: " + av.calcularSueldo("10053081056")); // asi essta en las indicaciones 
-		System.out.println("Sueldo vendedor mixto: " + av.calcularSueldo("0012345678"));
-		
-		
-		//		•	Probar con una cédula inexistente 
+			VendedorMixto vendedorMixto = new VendedorMixto("0012345678");
+			vendedorMixto.setComisionPorVenta(1);
+			vendedorMixto.setNumeroVentas(50);
+			vendedorMixto.setSueldoFijo(450);
+			
+			//	Agregar los vendedores usando el método agregar 
+			av.agregar(vendedor);
+			av.agregar(vendedorComision);
+			av.agregar(vendedorMixto);
+			
+			//Probar el método calcularSueldo con los tres vendedores 
+			System.out.println("Sueldo vendedor normal: " + av.calcularSueldo("1753081056"));
+			System.out.println("Solo Comision --- > vendedor comision: " + av.calcularSueldo("10053081056")); // asi essta en las indicaciones 
+			System.out.println("Sueldo vendedor mixto: " + av.calcularSueldo("0012345678"));
+			
+			
+			//		•	Probar con una cédula inexistente 
 
-		System.out.println("Cedula inexistente: " + av.calcularSueldo("0000888888"));
+			System.out.println("Cedula inexistente: " + av.calcularSueldo("0000888888"));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+		
+
+		
 	}
 
 }
